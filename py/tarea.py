fa443c9e7fc99a6e111c5e5055667f8710bc6443
Brian Gabriel Hurtado Moreno <140124803+brianshoegaze04@users.noreply.github.com>
2023-09-10 05:37:00 -0500

    Tarea Resuelta de Mary

---
+++ py/tarea.py
@@ -0,0 +1,5 @@
+nombre = str(input('digita tu nombre '))
+edad = int(input('digita tu edad '))
+pais = str(input('digita tu pais '))
+mensaje = f"Bienvenido {nombre} nos alegra que vinieras de {pais} con una edad de {edad}"
+print(mensaje)
